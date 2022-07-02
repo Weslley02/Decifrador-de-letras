0bfe61273fe5a01e0116976c000f6a56439101f6
Weslley Borges <80173269+Weslley02@users.noreply.github.com>
2022-07-02 00:11:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int i;
+    Word word = new Word();
+    word.set Word("O lanche está dois reais!");
+    String contrario[]=word.getWord().split(" ");
+    For(i=contrario - 1;i>=0;i--){
+    System.out.println(contrario[i] + contrario[i].length ());
+    
+    }
+  }
+}
---
+++ Word.java
@@ -0,0 +1,14 @@
+package word;
+public class Word {
+    private String text;
+    private int letters;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String newText) {
+        this.text = newText;
+    }
+
+}
